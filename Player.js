e1a3510de0ab8085a6f128521d694e306418d1ed
heheas <hehe.andrew@gmail.com>
2025-04-23 12:10:47 -0400

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,35 +1,37 @@
-private var _playerID = null;
-private var _playerName = "";
-
-/** Getters and Setters **/
-
-//Player ID
-function getPlayerID() {
-  return _playerID;
-}
-function setPlayerID(var playerID) {
-  _playerID = !playerID ? playerID : nextID();
-}
-
-//Player Name
-function getPlayerName() {
-  return _playerName;
-}
-function setPlayerName(var playerName) {
-  _playerName = !_playerName ? playerName : nextID();
-}
-/** End Getters and Setters **/
-
-//Init Function
-function init(var playerID = null, var playerName = null) {
-  _playerID = !playerID ? playerID : nextID();
-  _playerName = !playerName ? playerName : randomName();
-}
-
-function nextID() {
-  return MATH.random() * 100;
-}
-
-function randomName() {
-  return "Player " + getPlayerID();
+export class Player {
+  private var _playerID = null;
+  private var _playerName = "";
+  
+  /** Getters and Setters **/
+  
+  //Player ID
+  function getPlayerID() {
+    return _playerID;
+  }
+  function setPlayerID(var playerID) {
+    _playerID = !playerID ? playerID : nextID();
+  }
+  
+  //Player Name
+  function getPlayerName() {
+    return _playerName;
+  }
+  function setPlayerName(var playerName) {
+    _playerName = !_playerName ? playerName : nextID();
+  }
+  /** End Getters and Setters **/
+  
+  //Init Function
+  function init(var playerID = null, var playerName = null) {
+    _playerID = !playerID ? playerID : nextID();
+    _playerName = !playerName ? playerName : randomName();
+  }
+  
+  function nextID() {
+    return MATH.random() * 100;
+  }
+  
+  function randomName() {
+    return "Player " + getPlayerID();
+  }
 }
